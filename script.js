7f5407cb12098db7939aed7f6bf3682b1b46e124
codelenov <codelenovo@tutanota.com>
2024-06-18 16:22:14 +0200

    MAKE JavaScript

---
+++ script.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const output = document.querySelector('div');
+let fizzBuzz = '';
+
+button.addEventListener('click', playFizzBuzz);
+
+function playFizzBuzz() {
+
+	for (let i=1; i<=input.value; i++) {
+
+		if (i % 3 === 0 && i % 5 === 0) {
+			fizzBuzz += `FizzBuzz<br />`;
+		} else if (i % 3 === 0) {
+			fizzBuzz += `Fizz<br />`;
+		} else if (i % 5 === 0) {
+			fizzBuzz += `Buzz<br />`;
+		} else {
+			fizzBuzz += `${i}<br />`;
+		}
+
+	}
+
+	output.innerHTML = fizzBuzz;
+
+}
+
